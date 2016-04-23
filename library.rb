3f9d842ad60c66cbb1d677bf0c2b815150c61f93
Den <man-dan@mail.ru>
2016-04-23 09:28:10 +0300

    Last method

--- library.rb
+++ library.rb
@@ -9,13 +9,27 @@ class Library
 	def initialize
 		@books, @orders, @readers, @authors = [], [], [], []
 	end
+	def who_takes_popular_books
+		three_books=three_most_popular_books
+		who=[]
+		@orders.each do |r| 
+			three_books.each {|k,v| who<<r.reader if k==r.book}
+		end
+		puts who.uniq!
+	end
 	def most_popular_book
+		arr=three_most_popular_books.max_by{|k,v| v}
+		puts arr
+	end
+	def three_most_popular_books
 		books=[]
 		@orders.each {|b| books<<b.book}
 		books.each_with_object(b=Hash.new(0)){|book| b[book]+=1}
-		b.sort {|a,b| b[1]<=>a[1]}.first[0]
+		c=Hash.new
+		b.sort {|a,b| b[1]<=>a[1]}.each_with_index{|(k,v),i| c[k]=v if i<3}
+		c
 	end
-	def stat_reader(book)
+	def stat_reader(book)#Who often takes the book
 		readers_stat = Hash.new(0)
 		@orders.each do |r| 
 			readers_stat[r.reader] +=1 if r.book==book
--- test.rb
+++ test.rb
@@ -5,9 +5,16 @@ lib = Library.new
 lib.add_order(Order.new("Potter","Den","21.04.16"))
 lib.add_reader(Reader.new("Petya","pet123@gmail.com","Dnepropetrovsk","Moljera","10"))
 lib.add_author(Author.new("Rowling","writer"))
-
+lib.add_author(Author.new("Arthur Ignatius Conan Doyle","writer"))
+lib.add_reader(Reader.new("Vasya","vas123@gmail.com","Dnepropetrovsk","Mostovaya","20"))
+lib.add_book(Book.new("The Lost World","Arthur Ignatius Conan Doyle"))
+lib.add_order(Order.new("RubyTutorial","Vasya","23.04.16"))
+lib.add_reader(Reader.new("Ivanov Ivan","iv124@gmail.com","Dnepropetrovsk","Red ctreet","88"))
+lib.add_order(Order.new("Lord of The Rings","Ivanov Ivan","23.04.16"))
 lib.save_lib
 =end
+
 lib.load_orders
 lib.stat_reader("Kobzar")
-puts lib.most_popular_book
+lib.most_popular_book
+lib.who_takes_popular_books
